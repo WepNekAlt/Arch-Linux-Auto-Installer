2d5835eac0c1aca69ccf4ad3431ec48c048e55de
WepNekAlt <wep.nek.alt@gmail.com>
2024-07-16 18:47:18 -0300

    Update Auto-Installer-VM.sh

--- Auto-Installer-VM.sh
+++ Auto-Installer-VM.sh
@@ -4,7 +4,8 @@ mkfs.ext4 /dev/vda2
 
 mount /dev/vda2 /mnt  
 mkdir /mnt/boot
-mount /dev/vda1 /mnt/boot/  
+mkdir /mnt/boot/efi
+mount /dev/vda1 /mnt/boot/efi  
 
 pacman-key --init 
 pacman-key --populate  
